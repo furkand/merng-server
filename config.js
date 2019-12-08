a1edaef7be51a09a12a0e54adcd29c9b075a72a5
Furkan Demirtürk <42823006+fdmrtrk@users.noreply.github.com>
2019-12-08 23:52:35 +0300

    I changed configration of mongo

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB:
-    'mongodb+srv://classsed:6lxxxlexaTslHLPe@cluster0-pcsru.mongodb.net/merng?retryWrites=true',
+    'mongodb+srv://furkandemirturk:<password>@cluster0-zezow.mongodb.net/test?retryWrites=true&w=majority',
   SECRET_KEY: 'some very secret key'
 };
