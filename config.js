97f35f312d483c0d9e2aff9209b3aa466d06477a
Furkan Demirtürk <42823006+fdmrtrk@users.noreply.github.com>
2019-12-08 23:56:18 +0300

    configration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB:
-    'mongodb+srv://furkandemirturk:<password>@cluster0-zezow.mongodb.net/test?retryWrites=true&w=majority',
+    'mongodb+srv://furkandemirturk:f4xu76NmhPQrVY6p@cluster0-zezow.mongodb.net/test?retryWrites=true&w=majority',
   SECRET_KEY: 'some very secret key'
 };
